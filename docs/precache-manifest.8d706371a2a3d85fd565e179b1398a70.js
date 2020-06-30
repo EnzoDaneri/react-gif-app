685f85b56bbb3bfe2eb68e7c83601dc9c78831cb
EnzoDaneri <enzodaneri@hotmail.com>
2020-06-30 15:38:47 -0300

    docs agregado

---
+++ docs/precache-manifest.8d706371a2a3d85fd565e179b1398a70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da1e3d6641b12dc07cdef8f3dbd2d1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64b1640b8aefc38528cf",
+    "url": "/static/css/main.a6639c98.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64b1640b8aefc38528cf",
+    "url": "/static/js/main.e3d71a54.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
